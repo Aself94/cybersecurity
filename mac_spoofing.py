e19db7e03bf4129009b168f0685e4bc2e982ae67
Aself94 <matteo.biagioni1994@gmail.com>
2025-07-09 09:08:40 +0200

    mac_spoofing.py

--- mac_spoofing.py
+++ mac_spoofing.py
@@ -2,7 +2,7 @@ from scapy.all import Ether,ARP,srp
 if __name__ =="__main__":
     broadcast="FF:FF:FF:FF:FF:FF"
     ether_layer=Ether(dst=broadcast)
-    ip_range="192.168.44.1/24"
+    ip_range="#" #inserisci il tuo IP al posto del cancelletto
     arp_layer=ARP(pdst=ip_range)
     packet=ether_layer/arp_layer
     ans,unans=srp(packet,iface="eth0",timeout=2)
